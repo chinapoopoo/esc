241ef2a5a7e284869f284d363ced609e79a75dcf
unknown <chinapoopoo@gmail.com>
2019-11-17 03:19:19 +0900

    [CREATE] meal 생성

---
+++ js/meal.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+  getInfo();
+});
+
+const checkCategory = item =>
+  item.category_id == 1 ||
+  item.category_id == 2 ||
+  item.category_id == 3 ||
+  item.category_id == 54;
+
+const trContent = (index, item) =>
+  `
+      <tr>
+          <th scope="row">${index + 1}</th>
+          <td>${item.name}</td>
+          <td>${item.phone}</td>
+          <td>${item.time == null ? "09:00~18:00" : item.time}</td>
+      </tr>
+      `;
+
+function getInfo() {
+  $.ajax({
+    url: "https://bluelim.dev/api/v3/info",
+    method: "GET",
+    dataType: "json"
+  })
+    .done(json => {
+      json
+        .filter(checkCategory)
+        .map((item, index) => $("tbody").append(trContent(index, item)));
+    })
+    .fail((xhr, status, errorThrown) => {
+      console.log(errorThrown);
+    })
+    .always((xhr, status) => {
+      console.log("request done");
+    });
+}
