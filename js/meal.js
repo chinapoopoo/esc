ded4eaefbc35de2241d200fee7f1fa3a6ce336f0
unknown <chinapoopoo@gmail.com>
2019-11-17 03:22:50 +0900

    [UPDATE] 야식 불러오기 완료

--- js/meal.js
+++ js/meal.js
@@ -2,32 +2,24 @@ $(document).ready(() => {
   getInfo();
 });
 
-const checkCategory = item =>
-  item.category_id == 1 ||
-  item.category_id == 2 ||
-  item.category_id == 3 ||
-  item.category_id == 54;
-
 const trContent = (index, item) =>
   `
       <tr>
           <th scope="row">${index + 1}</th>
           <td>${item.name}</td>
           <td>${item.phone}</td>
-          <td>${item.time == null ? "09:00~18:00" : item.time}</td>
+          <td>${item.startTime}~${item.endTime}</td>
       </tr>
       `;
 
 function getInfo() {
   $.ajax({
-    url: "https://bluelim.dev/api/v3/info",
+    url: "https://bluelim.dev/api/snack",
     method: "GET",
     dataType: "json"
   })
     .done(json => {
-      json
-        .filter(checkCategory)
-        .map((item, index) => $("tbody").append(trContent(index, item)));
+      json.map((item, index) => $("tbody").append(trContent(index, item)));
     })
     .fail((xhr, status, errorThrown) => {
       console.log(errorThrown);
