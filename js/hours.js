67b20b39045051ca3858ad22f3fabd202666df21
Poolyum <chinapoopoo@gmail.com>
2019-12-04 23:51:12 +0900

    [UPDATE] 영어로 번역

--- js/hours.js
+++ js/hours.js
@@ -12,7 +12,7 @@ const trContent = (index, item) =>
   `
     <tr>
         <th scope="row">${index + 1}</th>
-        <td>${item.name}</td>
+        <td>${translate[item.id]}</td>
         <td>${item.phone}</td>
         <td>${item.time == null ? "09:00~18:00" : item.time}</td>
     </tr>
@@ -33,3 +33,49 @@ function getInfo() {
       console.log(errorThrown);
     });
 }
+
+const translate = {
+  22: "GLS",
+  32: "ICT",
+  17: "Security office",
+  23: "Management and Economics",
+  29: "Spatial Environment System Engineering",
+  31: "Engineering Education Innovation Center",
+  3: "Academy supporting team",
+  18: "Education development center",
+  40: "International hall office",
+  20: "International studies, Languages and literature",
+  6: "International affairs",
+  28: "Mechanical and Control Engineering",
+  39: "Lothem Hall office",
+  43: "Mom’s Kitchen",
+  13: "Convenience store",
+  45: "Bus lost-and-found",
+  30: "Law",
+  36: "Bethel hall office",
+  41: "Clinic",
+  7: "Welfare society",
+  35: "Vision hall office",
+  47: "Counseling Psychology and Social Welfare",
+  25: "Life science",
+  34: "Dormitory management office",
+  14: "Book store",
+  16: "Dry-Cleaning and Laundry",
+  5: "Facility management team",
+  26: "Communication arts and science",
+  19: "GLC(Global language center)",
+  15: "Post office",
+  38: "Grace hall office",
+  46: "Rent bus lost-and-found",
+  27: "Computer Science and Electrical Engineering",
+  12: "Information and service promotion office",
+  33: "Creative Convergence education center",
+  37: "Creation hall office",
+  4: "General staff personnel team",
+  8: "Image solution(print&fax)",
+  24: "Creative Convergence Education",
+  42: "Farm’s Barley",
+  11: "Student Experience Development Team",
+  44: "Cafeteria",
+  2: "Student support team"
+};
--- js/meal.js
+++ js/meal.js
@@ -6,7 +6,7 @@ const trContent = (index, item) =>
   `
       <tr>
           <th scope="row">${index + 1}</th>
-          <td>${item.name}</td>
+          <td>${translate[item.id]}</td>
           <td>${item.phone}</td>
           <td>${item.startTime}~${item.endTime}</td>
       </tr>
@@ -25,3 +25,20 @@ function getInfo() {
       console.log(errorThrown);
     });
 }
+
+const translate = {
+  14: "Jibungwiepadak & ttobagi (지붕위에 파닭&또박이)",
+  5: "Dondoni seoksoe hanpan bulgogi (돈도니 석쇠한판 불고기)",
+  23: "Love me soup tteokbokki& large cutlet(러브미 국물떡볶이&왕돈까스)",
+  2: "Ttangttang chicken(Heunghae), 땅땅치킨(흥해점)",
+  4: "Samchonne jjimdak 삼촌네 찜닭",
+  28: "Ugong fish (Changpo), 우공피쉬 초밥(창포점)",
+  12: "Shinsegae dakbal 신세계 닭발",
+  32: "Tosirae jokbal/bossam/dosirak 토시래 족발/보쌈/도시락",
+  26: "Manyeo Jokbal 마녀족발",
+  30: "Hosigi dumari chicken (Yangdeok), 호식이두마리치킨(양덕점)",
+  31: "Ugong fish (Yangdeok), 우공피쉬(양덕점)",
+  11: "Ttangttang chicken(Yangdeok), 땅땅치킨(양덕점)",
+  8: "Michyeo beorin padak 미쳐버린 파닭",
+  10: "Chiko padak 치코파닭"
+};
